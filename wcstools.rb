016cfb845bb6f608852f41bb4e6070d6877b3ba7
Dustin Lang <dstndstn@gmail.com>
2012-09-25 16:09:13 -0400

    Clean up and comment

--- wcstools.rb
+++ wcstools.rb
@@ -1,8 +1,11 @@
 require 'formula'
 
+# The wcstools-3.8.6 tarball seems to include a "Man" directory, and a
+# "man -> Man" symlink.  On a typical cases-insensitive HFS+ Mac
+# volume this causes trouble.  Tell "tar" not to extract the second
+# "man".
 class TarIgnoreDuplicates < CurlDownloadStrategy
   def stage
-    #Homebrew.system('/usr/bin/tar', 'xkf', @tarball_path)
     safe_system '/usr/bin/tar', 'xkf', @tarball_path, '--exclude', '*/man'
     chdir
   end
@@ -11,22 +14,18 @@ end
 class Wcstools < Formula
   homepage 'http://tdc-www.harvard.edu/wcstools/'
   url 'http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.6.tar.gz', :using => TarIgnoreDuplicates
-  #version '3.8.6'
   sha1 '2c42eb314d422ccd9c3f1999ac2642e9de480b4c'
 
   def install
     system "make all"
-
+    # There doesn't seem to be a Makefile install target, so DIY.
+    # Don't install the libwcs & libned includes directly in include/
+    # because the "fitsfile.h" (and maybe others) conflict with
+    # cfitsio.
     bin.install Dir['bin/*']
     lib.install 'libwcs/libwcs.a', 'libned/libned.a'
-    #mkdir '#{prefix}/include/libwcs'
-    #mkdir '#{prefix}/include/libned'
     (include + 'libwcs').install Dir['libwcs/*.h']
     (include + 'libned').install Dir['libned/*.h']
     man1.install Dir['man/man1/*']
-  end
-
-  def test
-    system "true"
   end
 end
