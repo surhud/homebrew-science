5084682b74df8965fb8cad5b2e738a73edeadeaf
Dustin Lang <dstndstn@gmail.com>
2012-10-01 11:49:41 -0400

    add TMV-CPP library

---
+++ tmv-cpp.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class TmvCpp < Formula
+  homepage 'http://code.google.com/p/tmv-cpp/'
+  url 'http://tmv-cpp.googlecode.com/files/tmv0.71.tar.gz'
+  sha1 'd407748ff4f6a0689edca006b070ff883ec59bbe'
+
+  depends_on 'scons' => :build
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+    system "scons"
+    system "scons install PREFIX=#{prefix}"
+  end
+end
