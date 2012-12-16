f70a21680de6a6bc0e2d88dd45be1e7d38771748
Dustin Lang <dstndstn@gmail.com>
2012-12-16 09:16:30 -0500

    New wcslib release (old one disappeared)

--- wcslib.rb
+++ wcslib.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wcslib < Formula
   homepage 'http://www.atnf.csiro.au/people/mcalabre/WCS/'
-  url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.14.tar.bz2'
-  sha1 '8c530c29866ca3414f62d27f439b3d5e6dcb39a1'
+  url 'ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-4.16.tar.bz2'
+  sha1 'd321ee3c82ecb14e447006ed475f3f0217aa589f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
