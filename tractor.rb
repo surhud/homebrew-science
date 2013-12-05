341544722dcc52610ab23f6ecbc3193b4c3a0fba
Dustin Lang <dstndstn@gmail.com>
2013-12-05 11:28:24 -0500

    add initial formula for the Tractor

---
+++ tractor.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Tractor < Formula
+  homepage 'https://github.com/dstndstn/tractor'
+
+  head 'https://github.com/dstndstn/tractor.git', :using => :git
+
+  depends_on 'swig' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'astrometry.net'
+
+  # from pip
+  depends_on 'pyfits' => :python
+  depends_on 'numpy' => :python
+
+  # this formula includes python bindings
+  depends_on :python => :recommended
+
+  def install
+    system "make"
+    system "python", "setup.py", "--prefix=#{prefix}"
+  end
+
+  def test
+    system "python -c 'import tractor'"
+  end
+end
