c4fe7510b5e070858ccf110980e62a6131c6d8cf
Dustin Lang <dstndstn@gmail.com>
2013-12-08 09:33:54 -0500

    put caveats in a separate caveats function

--- gal-sim.rb
+++ gal-sim.rb
@@ -15,30 +15,31 @@ class GalSim < Formula
   depends_on 'pyfits' => :python
   depends_on 'numpy' => :python
 
+  def pyver
+    IO.popen("python -c 'import sys; print sys.version[:3]'").read.strip
+  end
+
   def install
     # This ought to be part of a standard homebrew install;
     # required so that homebrew creates symlinks
     #   lib/pythonX.Y/galsim -> Cellar/gal-sim/0.2/lib/pythonX.Y/galsim
     # rather than
     #   lib/pythonX.Y -> Cellar/gal-sim/0.2/lib/pythonX.Y
-    pyver = ''
-    IO.popen("python -c 'import sys; print sys.version[:3]'") {|pv_io|
-        pyver = pv_io.read.strip
-    }
-    ohai "Python version is *#{pyver}*"
+    ohai "Python version is #{pyver}"
     mkdir_p "#{HOMEBREW_PREFIX}/lib/python#{pyver}"
-
     system "scons"
     system "scons install PREFIX=#{prefix} PYPREFIX=#{lib}/python#{pyver}"
-
-    ohai ""
-    ohai "The GalSim installer may warn you that #{lib}/python isn't in your python search path."
-    ohai "You may want to add all Homebrew python packages to the default paths by running:"
-    ohai "   sudo bash -c 'echo \"/usr/local/lib/python\" >> \\\\"
-    ohai "     /Library/Python/#{pyver}/site-packages/homebrew.pth'"
-    ohai "Which will create the file   /Library/Python/#{pyver}/site-packages/homebrew.pth"
-    ohai "with contents:"
-    ohai "  /usr/local/lib/python#{pyver}"
-    ohai ""
   end
+
+  def caveats; <<-EOS.undent
+    The GalSim installer may warn you that #{lib}/python isn't in your python search path.
+    You may want to add all Homebrew python packages to the default paths by running:
+       sudo bash -c 'echo \"/usr/local/lib/python\" >> \\\\
+         /Library/Python/#{pyver}/site-packages/homebrew.pth'
+    Which will create the file   /Library/Python/#{pyver}/site-packages/homebrew.pth
+    with contents:
+      /usr/local/lib/python#{pyver}
+    EOS
+  end
+
 end
