ba1d7704b88342af1b6ef53247d73559fecc41e7
Dustin Lang <dstndstn@gmail.com>
2013-12-18 17:02:14 -0500

    bump ceres-solver to version 1.8.0

--- ceres-solver.rb
+++ ceres-solver.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class CeresSolver < Formula
   homepage 'http://code.google.com/p/ceres-solver/'
-  url 'http://ceres-solver.googlecode.com/files/ceres-solver-1.7.0.tar.gz'
-  sha1 '412af6f267852554eae47d3bfe412607ee41c7c5'
+  url 'http://ceres-solver.googlecode.com/files/ceres-solver-1.8.0.tar.gz'
+  sha1 '8a67268d995b8351bd5ee5acf1eebff910028e7e'
   head 'https://ceres-solver.googlesource.com/ceres-solver.git'
 
   depends_on 'cmake' => :build
